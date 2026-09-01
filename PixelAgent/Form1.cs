4cf253700706ccac7c4edb5bbc5f30bc879e3c50
ahmdkaml <ahmdkaml2006@gmail.com>
2026-09-01 18:33:20 +0300

    feat: create initial WebView2 app

---
+++ PixelAgent/Form1.cs
@@ -0,0 +1,9 @@
+namespace PixelAgent;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
---
+++ PixelAgent/Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.Web.WebView2.WinForms;
+
+namespace PixelAgent;
+
+internal static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+
+        var form = new Form
+        {
+            Text = "Pixel Agent",
+            Width = 1000,
+            Height = 700
+        };
+
+        var webView = new WebView2
+        {
+            Dock = DockStyle.Fill
+        };
+
+        form.Controls.Add(webView);
+
+        form.Shown += async (_, _) =>
+        {
+            try
+            {
+                await webView.EnsureCoreWebView2Async();
+
+                webView.NavigateToString("""
+                    <!DOCTYPE html>
+                    <html>
+                    <body>
+                        <h1>Hello World</h1>
+                    </body>
+                    </html>
+                    """);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    ex.ToString(),
+                    "Pixel Agent Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
+        };
+
+        Application.Run(form);
+    }
+}
