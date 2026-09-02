5e18524f9d0fc183203a14a9fef53288bde4eb25
ahmdkaml <ahmdkaml2006@gmail.com>
2026-09-02 11:57:34 +0300

    feat: load design image into canvas

--- PixelAgent/Host/WebViewHost.cs
+++ PixelAgent/Host/WebViewHost.cs
@@ -1,5 +1,8 @@
 using Microsoft.Web.WebView2.Core;
 using Microsoft.Web.WebView2.WinForms;
+using PixelAgent.Services;
+
+using System.Text.Json;
 
 namespace PixelAgent.Host;
 
@@ -7,9 +10,12 @@ public class WebViewHost
 {
     private readonly WebView2 _webView;
 
-    public WebViewHost(WebView2 webView)
+    private readonly DesignImageService _designService;
+
+    public WebViewHost(WebView2 webView, DesignImageService designService)
     {
         _webView = webView;
+        _designService = designService;
     }
 
     public void Initialize()
@@ -17,10 +23,32 @@ public class WebViewHost
         _webView.CoreWebView2.WebMessageReceived += OnWebMessageReceived;
     }
 
-    private void OnWebMessageReceived(
+    private async void OnWebMessageReceived(
         object? sender,
         CoreWebView2WebMessageReceivedEventArgs e)
     {
-        MessageBox.Show("WebViewHost received a message!");
+        var message = e.WebMessageAsJson;
+
+        using var document = JsonDocument.Parse(message);
+
+        var action = document.RootElement
+            .GetProperty("action")
+            .GetString();
+
+        switch (action)
+        {
+            case "open_design_dialog":
+
+                var imageData = _designService.OpenDesignDialog();
+
+                if (imageData != null)
+                {
+                    var script = $"window.setDesignImage({JsonSerializer.Serialize(imageData)});";
+
+                    await _webView.CoreWebView2.ExecuteScriptAsync(script);
+                }
+
+                break;
+        }
     }
 }
